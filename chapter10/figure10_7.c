8774e0d9f0f374bf40a26f998319a8a745973915
yejinp <ye895937@126.com>
2018-03-13 22:07:51 +0800

    add figure10_7.c

---
+++ chapter10/figure10_7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void
+sig_alrm(int signo)
+{
+	/* nothing to do, just return to wake up the pause */
+}
+
+unsigned int
+sleep1(unsigned int nsecs)
+{
+
+	if(signal(SIGALRM, sig_alrm) == SIG_ERR)
+		return (nsecs);
+
+	alarm(nsecs);
+	pause();
+	return (alarm(0));
+}
+
+
+int
+main(int argc, char *argv[])
+{
+
+	if(argc == 2) 
+		sleep1(atoi(argv[1]));
+
+}
