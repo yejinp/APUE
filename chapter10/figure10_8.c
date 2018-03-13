afc54da86277e63fd839ae13ad5319ef5c9a6fdc
yejinp <ye895937@126.com>
2018-03-13 22:19:19 +0800

    add figure10_8.c

---
+++ chapter10/figure10_8.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <setjmp.h>
+
+static jmp_buf	env_alrm;
+
+static void
+sig_alrm(int signo)
+{
+	longjmp(env_alrm, 1);
+	/* nothing to do, just return to wake up the pause */
+}
+
+unsigned int
+sleep1(unsigned int nsecs)
+{
+
+	if(signal(SIGALRM, sig_alrm) == SIG_ERR)
+		return (nsecs);
+	if(setjmp(env_alrm) ==0) {
+		alarm(nsecs);
+		pause();
+	}
+	return (alarm(0));
+}
+
+
+int
+main(int argc, char *argv[])
+{
+
+	if(argc == 2) 
+		sleep1(atoi(argv[1]));
+
+}
