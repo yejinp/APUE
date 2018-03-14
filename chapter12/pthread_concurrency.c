c62b288162c415a077275405730fa508c7e1a3fe
yejinp <ye895937@126.com>
2018-03-14 21:26:53 +0800

    add pthread_concurrency.c

---
+++ chapter12/pthread_concurrency.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+static void *test_thd(void *argv)
+{
+	int err;
+	printf("in the thread.\n");
+	printf("concurrency:%d\n",pthread_getconcurrency());
+	err = pthread_setconcurrency(3);
+	if(err) {
+		printf("set concurrency to 3 error.\n");
+	} else {
+		printf("set concurrency to 3 ok.\n");
+	}
+	
+}
+
+int main()
+{
+
+	pthread_t	tid;
+	pthread_attr_t	attr;
+
+	printf("in the main thread.\n");
+	if(pthread_attr_init(&attr) != 0)
+	{
+		printf("attr init failed.\n");
+		exit(1);
+	}
+
+	if(pthread_create(&tid, &attr, &test_thd, NULL) != 0)
+	{
+		printf("thread create error");
+		exit(2);
+	}
+	sleep(5);
+
+	exit(0);
+}
