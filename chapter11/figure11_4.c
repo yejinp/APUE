771e2317d1025885db129aab4ef1f3f271dc8bd1
yejinp <ye895937@126.com>
2018-03-14 22:05:18 +0800

    add figure11_4.c

---
+++ chapter11/figure11_4.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+
+struct foo {
+	int a, b, c, d;
+};
+
+void 
+printfoo(const char *s, const struct foo *fp)
+{
+
+	printf(s);
+	printf("\tstruct at 0x%p", fp);
+	printf("\tfoo.a = %d\n", fp->a);
+	printf("\tfoo.b = %d\n", fp->b);
+	printf("\tfoo.c = %d\n", fp->c);
+	printf("\tfoo.d = %d\n", fp->d);
+}
+
+void *
+thr_fn1(void *arg)
+{
+	struct foo foo = {1, 2, 3, 4};
+	printfoo("thread 1:", &foo);
+	pthread_exit((void *)&foo);
+}
+
+void *thr_fn2(void *arg)
+{
+	printf("thread 2: ID is %d\n", pthread_self());
+	pthread_exit((void *)0);
+}
+
+int main()
+{
+	int 	err;
+	pthread_t	tid1, tid2;
+	struct foo	*fp;
+	err = pthread_create(&tid1, NULL, thr_fn1, NULL);
+	if(err) {
+		printf("can't create thread 1: %s\n", strerror(err));
+		exit(err);
+	}
+
+	err = pthread_join(tid1, (void *)&fp);
+	if(err) {
+		printf("cat't join with thread 1: %s\n", strerror(err));
+	}
+	
+	sleep(1);
+
+	printf("parent starting the second thread\n");
+	err = pthread_create(&tid2, NULL, thr_fn2, NULL);
+	if(err) {
+		printf("can't create thread 2: %s\n", strerror(err));
+		exit(err);
+	}
+	sleep(1);
+	printfoo("parent:\n", fp);
+	exit(0);
+}
