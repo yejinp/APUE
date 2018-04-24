e9c1cd4b63f7e694154f085d816a5e4fe37b7541
yejinp <ye895937@126.com>
2018-04-24 22:12:25 +0800

    fix a problem

--- chapter10/figure10_2.c
+++ chapter10/figure10_2.c
@@ -28,8 +28,8 @@ sig_usr(int signo)
 {
 	if(signo == SIGUSR1) {
 		printf("received SIGUSR1\n");
-	} else if(signo == SIGUSR1) {
-		printf("received SIGUSR1\n");
+	} else if(signo == SIGUSR2) {
+		printf("received SIGUSR2\n");
 	} else 
 		printf("received signal %d\n", signo);
 }
