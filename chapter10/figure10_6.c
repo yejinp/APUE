7fe7e7c84d664838509a9299d69ec5445a515698
yejinp <ye895937@126.com>
2018-04-25 07:21:40 +0800

    add figure10_6.c

---
+++ chapter10/figure10_6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void sig_cld(int);
+
+int 
+main()
+{
+	pid_t	pid;
+
+	if(signal(SIGCLD, sig_cld) == SIG_ERR) {
+		perror("signal error");
+	} 
+
+	if((pid = fork()) < 0) {
+		perror("fork error");
+	} else if(pid == 0) {
+		sleep(2);
+		_exit(0);
+	}
+
+	pause();
+	exit(0);
+}
+
+static void
+sig_cld(int signo) 
+{
+	pid_t	pid;
+	int	status;
+
+	printf("SIGCLD Received\n");
+	if(signal(SIGCLD, sig_cld) == SIG_ERR)
+		perror("signal eror");
+
+	if((pid = wait( &status)) < 0) 
+		perror("wait error");
+
+	printf("pid = %d\n", pid); 
+}
