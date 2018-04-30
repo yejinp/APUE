ad40313e567135abe1fe2712cf4039e8b5b81f66
yejinp <ye895937@126.com>
2018-05-01 07:35:19 +0800

    change setsid.c

--- chapter09/setsid.c
+++ chapter09/setsid.c
@@ -8,7 +8,7 @@ main(int argc, char *argv[])
 {
 	printf("sid:%d\n", getsid(0));
 	if(setsid() < 0) {
-		perror("set sid faile");
+		perror("set sid failed");
 	} else {
 		printf("set sid ok.\n");
 		printf("after set. pid:%d sid:%d\n", getpid(), getsid(0));
