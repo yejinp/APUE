423dcfd1b4ef06f2c4038d45af1d24ab53b79c1a
yejinp <ye895937@126.com>
2018-05-03 04:16:25 -0700

    add figure10.19

---
+++ chapter10/figure10_19.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <signal.h>
+
+typedef	void (Sigfunc(int));
+
+Sigfunc  *
+signal(int signo, Sigfunc *func)
+{
+	struct sigaction	act, oact;
+	act.sa_handler = func;
+	sigmeptyset(&act.sa_mask);
+	act.sa_flags = 0;
+#ifdef	SA_INTERRUPT
+		act.sa_flags |= SA_INTERRUPT;
+#endif	
+
+	if(sigaction(signo, &act, &oact) < 0) {
+		return (SIG_ERR);
+	}
+
+	return	(oact.sa_handler);
+}
